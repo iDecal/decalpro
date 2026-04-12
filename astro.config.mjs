96152ed2ad2153f3c655918df0e5a431bb37ed3c
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-12 23:10:59 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,18 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   // Add your domain here
-   site: 'https://lexingtonthemes.com',
+   site: 'https://thicongdecal.pro',
   integrations: [ sitemap()]
+});
+import { defineConfig } from 'astro/config';
+import path from 'path';
+
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
