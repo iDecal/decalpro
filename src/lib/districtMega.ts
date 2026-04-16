23d46597615a2463b4c59fee2e17cad15c7b82e9
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-16 22:45:51 +0700

    Create districtMega.ts

---
+++ src/lib/districtMega.ts
@@ -0,0 +1,25 @@
+export const districtMega = [
+  {
+    title: "Khu trung tâm",
+    districts: [
+      { name: "Quận 1", slug: "quan-1" },
+      { name: "Quận 3", slug: "quan-3" },
+      { name: "Quận 5", slug: "quan-5" }
+    ]
+  },
+  {
+    title: "Khu Đông",
+    districts: [
+      { name: "Thủ Đức", slug: "thu-duc" },
+      { name: "Quận 9", slug: "quan-9" },
+      { name: "Bình Thạnh", slug: "binh-thanh" }
+    ]
+  },
+  {
+    title: "Khu Nam",
+    districts: [
+      { name: "Quận 7", slug: "quan-7" },
+      { name: "Nhà Bè", slug: "nha-be" }
+    ]
+  }
+]
